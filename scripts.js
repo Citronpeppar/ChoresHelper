a3de89c9e8efc4f891d55758540dedcf47ec9ec9
Liv Nachenius <liv_n@mac.com>
2019-04-01 15:14:04 +0200

    Startup

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+var chores = [
+	"Vattna blommorna",
+	"Klappa katten"
+
+];
+
+//* Puts the item the user writes in the chores array.
+chores.push(prompt("Enter a Chore"));
+
+for (var i = 0; i < chores.length; i++) {
+	chores[i]
+}
+
+document.getElementById('list').innerHTML+= chores;
+
+<button onclick="myFunction()">Click me</button>
+
+console.log (chores);
