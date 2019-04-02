dd7cb6f32694bf397334785e8c03c59b145d9eac
Liv Nachenius <liv_n@mac.com>
2019-04-02 14:14:56 +0200

    added click to add chore

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
 
-var chores = [
+var savedChores = [
 	"Vattna blommorna",
 	"Klappa katten"
 
 ];
 
+function displayNewChore()
+{
+  var theChore = document.getElementById("enter").value;
+  if (theChore == "" || theChore.length == 0)
+  {
+     return false; //stop the function since the value is empty.
+  }
+  savedChores.push(theChore);
+
+document.getElementById('list').children[0].innerHTML += "<li>"+savedChores [savedChores.length-1] + "</li>";
+};
+
+
+//*console.log (savedChores);
+
+
+
+
+
 //* Puts the item the user writes in the chores array.
-chores.push(prompt("Enter a Chore"));
+//*chores.push(prompt("Enter a Chore"));
 
-for (var i = 0; i < chores.length; i++) {
-	chores[i]
-}
-
-document.getElementById('list').innerHTML+= chores;
-
-<button onclick="myFunction()">Click me</button>
-
-console.log (chores);
+//* A loop for the array. Use to write the list on the page.
+//*for (var i = 0; i < chores.length; i++) {
+//*	chores[i]
